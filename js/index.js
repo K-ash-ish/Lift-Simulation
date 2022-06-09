d9df890ee3b8cd82f398a539659ecae2950a21fd
K-ash-ish <kashishsondhiya@gmail.com>
2022-06-09 19:05:50 +0530

    simple lift open and close function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 
+const liftBtn = document.querySelectorAll(".lift-btn");
+const doors = document.querySelectorAll(".door")
+const leftDoor = document.querySelector(".left-door");
+const rightDoor = document.querySelector(".right-door");
+liftBtn.forEach(btn => {
+    btn.addEventListener("click", (e)=>{
+       leftDoor.classList.add("left-door-close");
+       rightDoor.classList.add("right-door-close");
+       setTimeout(() => {
+        leftDoor.classList.remove("left-door-close");
+        rightDoor.classList.remove("right-door-close");
+       }, 5000);
+    })
+});
 
 
