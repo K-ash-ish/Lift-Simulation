2a7ca9246f9ae30cd5425fd2f86f3237cdec0b17
K-ash-ish <kashishsondhiya@gmail.com>
2022-06-11 14:32:37 +0530

    lift movement

--- js/index.js
+++ js/index.js
@@ -3,15 +3,34 @@ const liftBtn = document.querySelectorAll(".lift-btn");
 const doors = document.querySelectorAll(".door")
 const leftDoor = document.querySelector(".left-door");
 const rightDoor = document.querySelector(".right-door");
+const lift = document.querySelector(".lift-doors")
 liftBtn.forEach(btn => {
     btn.addEventListener("click", (e)=>{
-       leftDoor.classList.add("left-door-close");
-       rightDoor.classList.add("right-door-close");
-       setTimeout(() => {
-        leftDoor.classList.remove("left-door-close");
-        rightDoor.classList.remove("right-door-close");
-       }, 5000);
+       openDoors();       
+       setTimeout(function(){
+           closeDoors();
+        }, 5000);
+       setTimeout(function(){
+           moveLiftUp(btn)
+           
+        }, 8000);
     })
 });
+function openDoors(){
+    leftDoor.classList.add("left-door-close");
+    rightDoor.classList.add("right-door-close");
+    console.log("door open")
+}
+function closeDoors(){
+    leftDoor.classList.remove("left-door-close");
+    rightDoor.classList.remove("right-door-close");
+    console.log("door close")
+}
+function moveLiftUp(btn){
+    lift.classList.add("lift")
+    console.log(btn)
+    console.log("lift moving to other floor...");    
+    
+}
 
 
