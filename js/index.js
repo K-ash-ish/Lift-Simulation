0216cd515d466165ea0bab009fde99a2516e5a11
K-ash-ish <kashishsondhiya@gmail.com>
2022-06-28 13:43:54 +0530

    simulation works perfectly with single lift for now will have to check for multiple lifts

--- js/index.js
+++ js/index.js
@@ -1,23 +1,19 @@
 
 let floorNumbers = 0;
 let lifts = 1;
+let floorQueue = [];
 const floors = document.querySelector(".floors");
 const addFloorBtn = document.querySelector(".add-floor");
 const addLiftBtn = document.querySelector(".add-lift");
 const banner = document.querySelector(".banner");
 const liftContainer = document.querySelector(".lift-container");
-const upBtn = document.querySelector(".up-btn");
-const downBtn = document.querySelector(".down-btn");
-const liftBtn = document.querySelectorAll(".lift-btn");
 const leftDoor = document.querySelector('.left-door');
 const rightDoor = document.querySelector(".right-door");
 const lift = document.querySelector(".lift-doors")
-let floorArray = [];
 //adding lifts
 addLiftBtn.addEventListener("click", ()=>{
     lifts++;
     let maxLifts;
-    console.log()
     if(window.innerWidth<600&& window.innerWidth > 400){
         maxLifts= 5;
         addLifts(maxLifts)
@@ -30,50 +26,6 @@ addLiftBtn.addEventListener("click", ()=>{
         addLifts(maxLifts);
     }
 })
-
-// adding floors
-addFloorBtn.addEventListener("click", ()=>{
-    floorNumbers++;
-    addFloor(floorNumbers)    ;
-})
-
-// door animation
-document.addEventListener("click", (e)=>{
-    if(e.target.classList.contains("lift-btn")){
-        // leftDoor.style.transform = "translateX(-85%)"
-        // rightDoor.style.transform = "translateX(85%)" 
-        // setTimeout(() => {
-        //     doorClose();   
-        // }, 5000);   
-        floorArray.push(e.target.classList.item(2))
-        console.log(floorArray) 
-        liftMovements(e.target.classList);
-    }
-})
-function doorClose(){
-    leftDoor.style.transform = "translateX(0)"
-    rightDoor.style.transform = "translateX(0)" 
-}
-function addFloor(floorNumbers){
-    let floorContainer = document.createElement("div");
-    floorContainer.classList.add("floor-container")
-    let floor = `
-            <div class="btn-container">
-                <button class="lift-btn up-btn   ${floorNumbers}">Up</button>
-                <button class="lift-btn down-btn ${floorNumbers}">Down</button>
-            </div>
-            <div class="lift-container">
-            </div>
-            
-            <div class="floor">
-                <div class="floor-base"></div>
-            </div>
-            <h3 class="floor-number">Floor ${floorNumbers}</h3>
-    `;
-    floorContainer.innerHTML= floor;
-    floors.prepend(floorContainer);
-}
-
 function addLifts(maxLifts){
     if(lifts<maxLifts){
         let liftDoor = document.createElement("div");
@@ -92,9 +44,90 @@ function addLifts(maxLifts){
         }, 1000);
     }
 }
-function liftMovements(e){
-    let move = e.item(2)*(-178);
-    lift.style.transition = "transform 2.5s linear"
-    lift.style.transform= "translateY(" + move + "px)";
+
+// adding floors
+addFloorBtn.addEventListener("click", ()=>{
+    floorNumbers++;
+    addFloor(floorNumbers)    ;
+})
+function addFloor(floorNumbers){
+    let floorContainer = document.createElement("div");
+    floorContainer.classList.add("floor-container")
+    let floor = `
+            <div class="btn-container">
+                <button class="lift-btn up-btn   ${floorNumbers}" data-floor = ${floorNumbers}>Up</button>
+                <button class="lift-btn down-btn ${floorNumbers}" data-floor = ${floorNumbers}>Down</button>
+            </div>
+            <div class="lift-container">
+            </div>
+            
+            <div class="floor">
+                <div class="floor-base"></div>
+            </div>
+            <h3 class="floor-number">Floor ${floorNumbers}</h3>
+    `;
+    floorContainer.innerHTML= floor;
+    floors.prepend(floorContainer);
 }
 
+// door animation
+document.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("lift-btn")){
+        // leftDoor.style.transform = "translateX(-85%)"
+        // rightDoor.style.transform = "translateX(85%)" 
+        // setTimeout(() => {
+        //     doorClose();   
+        // }, 5000);   
+        targetFloor = e.target.classList.item(2);
+        checkLiftStatus(targetFloor);
+        // liftMovements(e.target.classList);
+    }
+})
+
+function checkLiftStatus(targetFloor){
+    console.log("Checking if lift is busy or not")
+
+    if(!lift.classList.contains("busy")){
+        console.log("Lift not busy moving to the floor")
+        moveLift(targetFloor);
+    }
+    else{
+        floorQueue.push(targetFloor)
+        console.log("lift is busy queuing the floor");
+    }
+}
+
+function moveLift(targetFloor){
+    console.log("moving to " + targetFloor);
+    let currentFloor = lift.dataset.currentfloor;
+    console.log(currentFloor + " current floor")
+    let duration = Math.abs(targetFloor - currentFloor) * 2
+    console.log(duration + " duration")
+    let move = targetFloor*(-178);
+    lift.style.transition = `transform ${duration}s linear`;
+    lift.style.transform= "translateY(" + move + "px)";
+    lift.classList.add("busy");
+    lift.dataset.currentfloor = targetFloor;
+    
+    setTimeout(() => {
+        leftDoor.style.transform = "translateX(-85%)"
+        rightDoor.style.transform = "translateX(85%)" 
+    }, duration * 1500);  
+
+    setTimeout(() => {
+        leftDoor.style.transform = "none"
+        rightDoor.style.transform = "none" 
+    }, 3000 +(1500 * duration)); 
+
+    setTimeout(() => {
+        lift.classList.remove("busy")
+        console.log(floorQueue);
+        if(floorQueue.length){
+            moveLift(floorQueue.shift());
+        }
+        else{
+            console.log("No more CALLLLLSS.....")
+        }
+    }, 6000 + ( duration * 1500));
+
+}
